71506f14189ca0e6be001974b3d3aaba00fc17ab
BIBIN OOMMEN MATHEW <bibin765@gmail.com>
2020-03-01 23:20:17 +0530

    markers added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL,{Marker} from 'react-map-gl';
 //import { render } from '@testing-library/react';
-import {Navbar,Nav } from 'react-bootstrap'
+import { Navbar,Nav } from 'react-bootstrap'
+import { listPlantEntries } from './API';
 import './App.css'
 
 function App() {
+  const [ plantEntries, setPlantEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -12,6 +14,14 @@ function App() {
     longitude: 80.0982,
     zoom: 3
   });
+
+  useEffect( () => {
+    (async () => {
+      const plantEntries = await listPlantEntries();
+      setPlantEntries(plantEntries);
+    })();
+   
+  }, []);
 
   return (
     <div>
@@ -28,7 +38,34 @@ function App() {
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       onViewportChange={setViewport}
       mapboxApiAccessToken={process.env.REACT_APP_ACCESS_TOKEN}
-    />
+      >
+      {
+        plantEntries.map(entry => (
+          <Marker 
+          latitude={10.8505} 
+          longitude={78.9629} 
+          >
+          
+          <svg viewBox="0 0 24 24"
+          style={{
+            height: `${3 * viewport.zoom}px`,
+            width: `${3 * viewport.zoom}px`,
+          }} 
+          stroke="#00ff00" 
+          stroke-width="1" 
+          fill="none" 
+          stroke-linecap="round" 
+          stroke-linejoin="round" 
+          class="css-i6dzq1">
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
+          </Marker>
+        ))
+
+      }
+      </ReactMapGL>
+      
     </div>
   );
 }
